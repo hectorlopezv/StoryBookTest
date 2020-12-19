ad059eef4cf2b40cbe650cbc6545e3f9918be723
Hector Vinicio Lopez Molinares <hectorvmlopez@gmail.com>
2020-12-19 12:50:22 -0500

    adding boiler plate code

---
+++ .storybook/main.ts
@@ -0,0 +1,19 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-knobs',
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        configureJSX: true,
+      },
+    },
+  ]
+}
